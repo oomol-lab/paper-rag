b2468f586c527ab8b2c09c09eef89586a4851844
TaoZeyu <i@taozeyu.com>
2024-11-20 14:51:40 +0800

    feat(browser): query pdf pages

--- browser/src/store/query_store.ts
+++ browser/src/store/query_store.ts
@@ -1,8 +1,42 @@
+import { val, derive, Val, ReadonlyVal } from "value-enhancer";
+import { message } from "antd";
+import { fetchJson } from "../utils";
+
+export type QueryStore$ = {
+  readonly isQuerying: ReadonlyVal<boolean>;
+  readonly queryResult: ReadonlyVal<QueryResult | null>;
+};
+
+export type QueryResult = {};
+
 export class QueryStore {
+  public readonly $: QueryStore$;
 
-  public constructor() {}
+  readonly #isQuerying$: Val<boolean> = val(false);
+  readonly #queryResult$: Val<QueryResult | null> = val<QueryResult | null>(null);
 
-  public query(query: string): void {
-    console.log(query);
+  public constructor() {
+    this.$ = {
+      isQuerying: derive(this.#isQuerying$),
+      queryResult: derive(this.#queryResult$),
+    };
+  }
+
+  public query(text: string): void {
+    const query = new URLSearchParams({
+      query: text,
+    });
+    this.#isQuerying$.set(true);
+    fetchJson<QueryResult>(`/api/query?${query}`)
+      .then((queryResults) => {
+        this.#queryResult$.set(queryResults);
+      })
+      .catch((error) => {
+        console.error(error);
+        message.error(error.message);
+      })
+      .finally(() => {
+        this.#isQuerying$.set(false);
+      });
   }
 }
--- browser/src/views/QueryPage.tsx
+++ browser/src/views/QueryPage.tsx
@@ -1,23 +1,49 @@
 import React from "react";
 import styles from "./QueryPage.module.less";
 
-import { Input } from "antd";
-import { QueryStore } from "../store";
+import { Skeleton, Input, Divider } from "antd";
+import { QueryResult, QueryStore } from "../store";
+import { useVal } from "use-value-enhancer";
 
 const { Search } = Input;
 
 export const QueryPage: React.FC<{}> = () => {
   const store = React.useMemo(() => new QueryStore(), []);
+  const isQuerying = useVal(store.$.isQuerying);
+  const queryResults = useVal(store.$.queryResult);
   const onSearch = React.useCallback(
     (query: string) => store.query(query),
     [],
   );
+  let tailView: React.ReactNode = null;
+
+  if (isQuerying) {
+    tailView = <Skeleton active />;
+  } else if (queryResults) {
+    tailView = <ResultDisplay result={queryResults} />;
+  }
   return <>
     <div className={styles["query-box"]}>
       <Search
         placeholder="输入你要搜索的内容"
         allowClear
         onSearch={onSearch} />
+      {tailView && <>
+        <Divider />
+        {tailView}
+      </>}
     </div>
   </>;
+};
+
+type ResultDisplayProps = {
+  readonly result: QueryResult;
+};
+
+const ResultDisplay: React.FC<ResultDisplayProps> = ({ result }) => {
+  return (
+    <div className={styles["query-result-box"]}>
+      {JSON.stringify(result, undefined, 2)}
+    </div>
+  );
 };
