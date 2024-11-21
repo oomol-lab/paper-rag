795304515c2dd361c0dc561df333125d22a00fff
Moskize91 <i@taozeyu.com>
2024-11-21 12:44:37 +0800

    feat(browser): support highlight of selected chunk of query results (#10)

--- browser/src/views/QueryPage.tsx
+++ browser/src/views/QueryPage.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import styles from "./QueryPage.module.less";
 
-import { Tag, Empty, Skeleton, Input, Divider, Descriptions } from "antd";
+import { Tag, Empty, Skeleton, Input, List } from "antd";
 import { useVal } from "use-value-enhancer";
-import { PDFPageItem, QueryResult, QueryStore } from "../store";
+import { useSearchParams } from "react-router-dom";
+import { PDFPageItem, QueryItem, QueryKeyword, QueryStore } from "../store";
 import { PDFTagLink } from "./Link";
+import { HighlightProvider } from "./HighlightTag";
 import { Text } from "./Text";
 
 const { Search } = Input;
@@ -12,60 +14,113 @@ const { Search } = Input;
 export const QueryPage: React.FC<{}> = () => {
   const store = React.useMemo(() => new QueryStore(), []);
   const isQuerying = useVal(store.$.isQuerying);
-  const queryResults = useVal(store.$.queryResult);
+  const items = useVal(store.$.items);
+  const keywords = useVal(store.$.keywords);
+  const [searchParams, setSearchParams] = useSearchParams();
+  let query: string | null | undefined = searchParams.get("query");
+
+  if (typeof query !== "string" || query.trim() === "") {
+    query = undefined;
+  }
+  React.useEffect(
+    () => {
+      if (typeof query === "string") {
+        store.query(query);
+      } else {
+        store.cleanQuery();
+      }
+    },
+    [store, query],
+  );
   const onSearch = React.useCallback(
-    (query: string) => store.query(query),
-    [],
+    (query: string) => {
+      if (query.trim() === "") {
+        setSearchParams({});
+      } else {
+        setSearchParams({ query });
+      }
+    },
+    [setSearchParams],
   );
   let tailView: React.ReactNode = null;
 
   if (isQuerying) {
     tailView = <Skeleton active />;
-  } else if (queryResults) {
-    tailView = <ResultDisplay result={queryResults} />;
+  } else if (items) {
+    tailView = (
+      <ResultDisplay
+        store={store}
+        items={items}
+        keywords={keywords} />
+    );
   }
-  return <>
-    <div className={styles["query-box"]}>
-      <Search
-        placeholder="输入你要搜索的内容"
-        allowClear
-        onSearch={onSearch} />
-      {tailView && <>
-        <Divider />
-        {tailView}
-      </>}
+  return (
+    <div className={styles.root}>
+      <div className={styles["query-box"]}>
+        <Search
+          placeholder="输入你要搜索的内容"
+          defaultValue={query}
+          allowClear
+          onSearch={onSearch} />
+      </div>
+      {tailView}
     </div>
-  </>;
+  );
 };
 
 type ResultDisplayProps = {
-  readonly result: QueryResult;
+  readonly store: QueryStore;
+  readonly items: readonly QueryItem[];
+  readonly keywords: readonly QueryKeyword[];
 };
 
-const ResultDisplay: React.FC<ResultDisplayProps> = ({ result }) => {
-  const { keywords, items } = result;
-  return (
-    <div className={styles["query-result-box"]}>
-      <div className={styles["keywords-bar"]}>
-        <label>关键词：</label>
-        {keywords.map((keyword, index) => (
-          <Tag key={`${index}`}>{keyword}</Tag>
-        ))}
+const ResultDisplay: React.FC<ResultDisplayProps> = ({ store, items, keywords }) => {
+  return <>
+    <Keywords
+      store={store}
+      keywords={keywords} />
+    <HighlightProvider keywords={keywords}>
+      <div className={styles["query-result-box"]}>
+        {items.length === 0 && (
+          <Empty
+            className={styles.empty}
+            description="没有搜索到内容" />
+        )}
+        {items.map((item, index) => {
+          if (!("content" in item)) {
+            // TODO: 对 PDF Metadata 本身的搜索
+            return null;
+          }
+          return (
+            <PDFPageCard key={`${index}`} item={item} />
+          );
+        })}
       </div>
-      {items.length === 0 && (
-        <Empty
-          className={styles.empty}
-          description="没有搜索到内容" />
-      )}
-      {items.map((item, index) => {
-        if (!("content" in item)) {
-          // TODO: 对 PDF Metadata 本身的搜索
-          return null;
-        }
-        return (
-          <PDFPageCard key={`${index}`} item={item} />
-        );
-      })}
+    </HighlightProvider>
+  </>;
+};
+
+type KeywordsProps = {
+  readonly store: QueryStore;
+  readonly keywords: readonly QueryKeyword[];
+};
+
+const Keywords: React.FC<KeywordsProps> = ({ store, keywords }) => {
+  const onChangeTag = React.useCallback(
+    (name: string, checked: boolean) => store.checkTag(name, checked),
+    [store],
+  );
+  return (
+    <div className={styles["keywords-bar"]}>
+      <label>关键词：</label>
+      {keywords.map((keyword, index) => (
+        <Tag.CheckableTag
+          key={`${index}`}
+          checked={keyword.checked}
+          onChange={checked => onChangeTag(keyword.name, checked)} >
+          {keyword.name}
+        </Tag.CheckableTag>
+      ))}
     </div>
   );
 };
@@ -78,22 +133,26 @@ const PDFPageCard: React.FC<PDFPageCardProps> = ({ item }) => {
   const { distance, pdf_files, content, segments } = item;
   return (
     <div className={styles["pdf-page-card"]}>
-      <Descriptions
-        layout="vertical"
-        items={[{
-          key: "1",
-          label: "文件",
-          children: pdf_files.map((pdf, index) => (
-            <PDFTagLink
-              key={`${index}`}
-              path={pdf.pdf_path}
-              page={pdf.page_index} />
-          )),
-        }, {
-          key: "2",
-          label: "距离",
-          children: distance,
-        }]} />
+      <List>
+        <List.Item>
+          <List.Item.Meta
+            title="距离"
+            description={distance} />
+          <List.Item.Meta
+            title="匹配片段"
+            description={segments.length} />
+        </List.Item>
+        <List.Item.Meta
+          title="文件"
+          description={
+            pdf_files.map((pdf, index) => (
+              <PDFTagLink
+                key={`${index}`}
+                path={pdf.pdf_path}
+                page={pdf.page_index} />
+            ))
+          } />
+      </List>
       <Text
         className={styles.text}
         content={content}
--- browser/src/views/Text.tsx
+++ browser/src/views/Text.tsx
@@ -1,16 +1,208 @@
 import React from "react";
+import styles from "./Text.module.less";
 
 import { HighlightSegment } from "../store";
+import { HighlightTag } from "./HighlightTag";
 
 export type TextProps = React.HTMLAttributes<HTMLDivElement> & {
   readonly content: string;
   readonly segments: readonly HighlightSegment[];
 };
 
+enum SegmentKind {
+  Background = 0,
+  Chunk = 1,
+  Highlight = 2,
+}
+
 export const Text: React.FC<TextProps> = ({ content, segments, ...rest }) => {
+  const dyeing = new Dyeing(content);
+  for (const { start, end, highlights } of segments) {
+    dyeing.dye(start, end - start, SegmentKind.Chunk, false);
+    for (const [hStart, hEnd] of highlights) {
+      dyeing.dye(start + hStart, hEnd - hStart, SegmentKind.Highlight, true);
+    }
+  }
   return (
     <div {...rest}>
-      {content}
+      {dyeing.split().map((fragments, i) => (
+        <TextParagraph
+          key={`${i}`}
+          fragments={fragments} />
+      ))}
     </div>
   );
 };
+
+type TextParagraphProps = {
+  readonly fragments: readonly Omit<Fragment, "offset">[][];
+};
+
+const TextParagraph: React.FC<TextParagraphProps> = ({ fragments }) => (
+  <div className={styles.paragraph}>
+    {fragments.map((fragments, i) => (
+      <TextLine
+        key={`${i}`}
+        fragments={fragments} />
+    ))}
+  </div>
+);
+
+type TextLineProps = {
+  readonly fragments: readonly Omit<Fragment, "offset">[];
+};
+
+const TextLine: React.FC<TextLineProps> = ({ fragments }) => (
+  <p>
+    {fragments.map((fragment, i) => {
+      switch (fragment.color) {
+        case SegmentKind.Background: {
+          return fragment.content;
+        }
+        case SegmentKind.Chunk: {
+          return (
+            <span
+              key={`${i}`}
+              className={styles.chunk}>
+              {fragment.content}
+            </span>
+          );
+        }
+        case SegmentKind.Highlight: {
+          return (
+            <HighlightTag
+              key={`${i}`}
+              keyword={fragment.keyword!}
+              className={styles.chunk}>
+              {fragment.content}
+            </HighlightTag>
+          );
+        }
+      }
+    })}
+  </p>
+);
+
+type Fragment = {
+  readonly content: string;
+  readonly offset: number;
+  readonly color: number;
+  readonly keyword: string | null;
+};
+
+enum Splitter {
+  LineBreak = 0,
+  Paragraph = 1,
+}
+
+class Dyeing {
+  #fragments: readonly Fragment[];
+
+  public constructor(content: string) {
+    this.#fragments = [{ content, offset: 0, color: 0, keyword: null }];
+  }
+
+  public dye(offset: number, length: number, color: number, markKeyword: boolean): void {
+    const newFragments: Fragment[] = [];
+
+    for (const fragment of this.#fragments) {
+      if (fragment.offset + fragment.content.length <= offset) {
+        newFragments.push(fragment);
+        continue;
+      }
+      if (fragment.offset >= offset + length) {
+        newFragments.push(fragment);
+        continue;
+      }
+      if (fragment.color >= color) {
+        newFragments.push(fragment);
+        continue;
+      }
+      const fragmentBegin = Math.max(fragment.offset, offset);
+      const fragmentEnd = Math.min(fragment.offset + fragment.content.length, offset + length);
+      const newContent = fragment.content.slice(fragmentBegin - fragment.offset, fragmentEnd - fragment.offset);
+
+      if (fragmentBegin > fragment.offset) {
+        newFragments.push({
+          content: fragment.content.slice(0, fragmentBegin - fragment.offset),
+          offset: fragment.offset,
+          color: fragment.color,
+          keyword: fragment.keyword,
+        });
+      }
+
+      let newKeyword = fragment.keyword;
+      if (markKeyword) {
+        newKeyword = newContent.replace(/[\s\n\r\x00-\x1F\x7F]+/g, "").trim();
+      }
+      newFragments.push({
+        content: newContent,
+        offset: fragmentBegin,
+        color,
+        keyword: newKeyword,
+      });
+
+      if (fragmentEnd < fragment.offset + fragment.content.length) {
+        newFragments.push({
+          content: fragment.content.slice(fragmentEnd - fragment.offset),
+          offset: fragmentEnd,
+          color: fragment.color,
+          keyword: fragment.keyword,
+        });
+      }
+    }
+    this.#fragments = newFragments;
+  }
+
+  public split(): Omit<Fragment, "offset">[][][] {
+    const paragraphs: Omit<Fragment, "offset">[][][] = [];
+    let paragraph: Omit<Fragment, "offset">[][] = [];
+    let line: Omit<Fragment, "offset">[] = [];
+
+    for (const item of this.#splitWithSplitters()) {
+      if (item === Splitter.LineBreak) {
+        if (line.length > 0) {
+          paragraph.push(line);
+          line = [];
+        }
+      } else if (item === Splitter.Paragraph) {
+        if (line.length > 0) {
+          paragraph.push(line);
+          line = [];
+        }
+        if (paragraph.length > 0) {
+          paragraphs.push(paragraph);
+          paragraph = [];
+        }
+      } else {
+        line.push(item);
+      }
+    }
+    return paragraphs;
+  }
+
+  *#splitWithSplitters(): Generator<Omit<Fragment, "offset"> | Splitter> {
+    for (const fragment of this.#fragments) {
+      // 令连续的多行回车变成标准的连续两个回车符（这些多行回车之间如果填充其他空白字符也会处理）
+      const content = fragment.content.replace(/\n\s+\n/g, "\n\n");
+      const cells = content.split("\n");
+
+      for (let i = 0; i < cells.length; ++i) {
+        const content = cells[i].trim();
+        if (content === "") {
+          yield Splitter.Paragraph;
+        } else {
+          yield {
+            content: cells[i].trim(),
+            color: fragment.color,
+            keyword: fragment.keyword,
+          }
+        }
+        if (i < cells.length - 1) {
+          yield Splitter.LineBreak;
+        }
+      }
+    }
+    yield Splitter.Paragraph;
+  }
+}
